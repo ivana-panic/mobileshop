56cf3c3a2112a977e1874d99e430b7cffbb9e51b
Bojan Panic <bojan@Bojans-MacBook-Pro.local>
2021-03-10 18:08:01 +0100

    add change

--- js/contact.js
+++ js/contact.js
@@ -1,90 +1,176 @@
+/*
 window.onload = function () {
   //regular expression
   //TODO dropodown, checkbox, radio buttons
-  document.addEventListener("DOMContentLoaded", function (event) {
-    document
-      .querySelector(".send-button")
-      .addEventListener("click", function check(event) {
-        event.stopImmediatePropagation();
-        event.preventDefault();
-        let errors = [];
-        var name = document.querySelector("#name").value;
-        var email = document.querySelector("#email-form").value;
-        var phone = document.querySelector("#phone-form").value;
-        var message = document.querySelector("#message").value;
-        var genders = document.getElementsByName("gender");
-        var genValues = false;
 
-        var inputName = document.querySelector("#name");
-        var inputEmail = document.querySelector("#email-form");
-        var inputPhone = document.querySelector("#phone-form");
-        var inputMessage = document.querySelector("#message");
+  document
+    .getElementById("submit")
+    .addEventListener("click", function check(event) {
+      event.stopImmediatePropagation();
+      event.preventDefault();
+      let errors = [];
+      var name = document.querySelector("#name").value;
+      var email = document.querySelector("#email-form").value;
+      var phone = document.querySelector("#phone-form").value;
+      var message = document.querySelector("#message").value;
+      var genders = document.getElementsByName("gender");
+      var genValues = false;
 
-        var regexName = /^[A-Z][a-z]+$/;
-        if (!regexName.test(name)) {
-          inputName.classList.remove("Correct");
-          inputName.classList.add("Incorrect");
-          errors.push(
-            "Please write a correct name! Example of a correctly written name: Maria Elias"
-          );
-        } else {
-          inputName.classList.remove("Incorrect");
-          inputName.classList.add("Correct");
+      var inputName = document.querySelector("#name");
+      var inputEmail = document.querySelector("#email-form");
+      var inputPhone = document.querySelector("#phone-form");
+      var inputMessage = document.querySelector("#message");
+
+      var regexName = /^[A-Z][a-z]+$/;
+      if (!regexName.test(name)) {
+        inputName.classList.remove("Correct");
+        inputName.classList.add("Incorrect");
+        errors.push(
+          "Please write a correct name! Example of a correctly written name: Maria Elias"
+        );
+      } else {
+        inputName.classList.remove("Incorrect");
+        inputName.classList.add("Correct");
+      }
+
+      var regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+      if (!regexEmail.test(email)) {
+        inputEmail.classList.remove("Correct");
+        inputEmail.classList.add("Incorrect");
+        errors.push(
+          "Please write a correct mail! Example of a correctly written mail address: mariaelias@hotmail.com"
+        );
+      } else {
+        inputEmail.classList.remove("Incorrect");
+        inputEmail.classList.add("Correct");
+      }
+
+      var regexPhone = /^06[01234569][\d]{6,7}$/;
+      if (!regexPhone.test(phone)) {
+        inputPhone.classList.remove("Correct");
+        inputPhone.classList.add("Incorrect");
+        errors.push(
+          "Please write a correct phone number! Example of a correctly written phone number: 064258879"
+        );
+      } else {
+        inputPhone.classList.remove("Incorrect");
+        inputPhone.classList.add("Correct");
+      }
+
+      var regexMessage = /^[A-ZČĆŽŠĐ][a-zčćžšđ]([\d \w]{5,100})$/;
+      if (!regexMessage.test(message)) {
+        inputMessage.classList.remove("Correct");
+        inputMessage.classList.add("Incorrect");
+        errors.push(
+          "Please write a correct message! Example of a correctly written message:A message of 5 to 100 characters"
+        );
+      } else {
+        inputPhone.classList.remove("Incorrect");
+        inputPhone.classList.add("Correct");
+      }
+
+      for (var i = 0; i < genders.length; i++) {
+        if (genders[i].checked == true) {
+          genValues = true;
         }
+      }
+      if (!genValues) {
+        error.push("Please choose the gender!");
+        return false;
+      }
 
-        var regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
-        if (!regexEmail.test(email)) {
-          inputEmail.classList.remove("Correct");
-          inputEmail.classList.add("Incorrect");
-          errors.push(
-            "Please write a correct mail! Example of a correctly written mail address: mariaelias@hotmail.com"
-          );
-        } else {
-          inputEmail.classList.remove("Incorrect");
-          inputEmail.classList.add("Correct");
-        }
-
-        var regexPhone = /^06[01234569][\d]{6,7}$/;
-        if (!regexPhone.test(phone)) {
-          inputPhone.classList.remove("Correct");
-          inputPhone.classList.add("Incorrect");
-          errors.push(
-            "Please write a correct phone number! Example of a correctly written phone number: 064258879"
-          );
-        } else {
-          inputPhone.classList.remove("Incorrect");
-          inputPhone.classList.add("Correct");
-        }
-
-        var regexMessage = /^[A-ZČĆŽŠĐ][a-zčćžšđ]([\d \w]{5,100})$/;
-        if (!regexMessage.test(message)) {
-          inputMessage.classList.remove("Correct");
-          inputMessage.classList.add("Incorrect");
-          errors.push(
-            "Please write a correct message! Example of a correctly written message:A message of 5 to 100 characters"
-          );
-        } else {
-          inputPhone.classList.remove("Incorrect");
-          inputPhone.classList.add("Correct");
-        }
-
-        for (var i = 0; i < genders.length; i++) {
-          if (genders[i].checked == true) {
-            genValues = true;
-          }
-        }
-        if (!genValues) {
-          error.push("Please choose the gender!");
-          return false;
-        }
-
-        var errorHtml = errors.map((error) => {
-          return $(`<div>${error}</div>`);
-        });
-        $("#errors").empty();
-        $.each(errorHtml, function (i, val) {
-          $("#errors").append(val);
-        });
+      var errorHtml = errors.map((error) => {
+        return $(`<div>${error}</div>`);
       });
-  });
+      $("#errors").empty();
+      $.each(errorHtml, function (i, val) {
+        $("#errors").append(val);
+      });
+    });
 };
+
+*/
+document.addEventListener("DOMContentLoaded", () => {
+  const regex = document.querySelector("#submit");
+
+  regex.addEventListener("click", function proveri(event) {
+    event.stopImmediatePropagation();
+    event.preventDefault();
+    var errors = [];
+    var name = document.querySelector("#form-name").value;
+    var email = document.querySelector("#form-email").value;
+    var phone = document.querySelector("#form-phone").value;
+    var text = document.querySelector("#form-text").value;
+    var genValues = false;
+
+    var inputName = document.querySelector("#form-name");
+    var inputEmail = document.querySelector("#form-email");
+    var inputPhone = document.querySelector("#form-phone");
+    var inputText = document.querySelector("#form-text");
+
+    var regexName = /^[A-Z][a-z]+$/;
+    if (!regexName.test(name)) {
+      inputName.classList.remove("Correct");
+      inputName.classList.add("Incorrect");
+      errors.push(
+        "Please write a correct name! Example of a correctly written name: Maria Elias"
+      );
+    } else {
+      inputName.classList.remove("Incorrect");
+      inputName.classList.add("Correct");
+    }
+
+    var regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+    if (!regexEmail.test(email)) {
+      inputEmail.classList.remove("Correct");
+      inputEmail.classList.add("Incorrect");
+      errors.push(
+        "Please write a correct mail! Example of a correctly written mail address: mariaelias@hotmail.com"
+      );
+    } else {
+      inputEmail.classList.remove("Incorrect");
+      inputEmail.classList.add("Correct");
+    }
+    var regexPhone = /^06[01234569][\d]{6,7}$/;
+    if (!regexPhone.test(phone)) {
+      inputPhone.classList.remove("Correct");
+      inputPhone.classList.add("Incorrect");
+      errors.push(
+        "Please write a correct phone number! Example of a correctly written phone number: 064258879"
+      );
+    } else {
+      inputPhone.classList.remove("Incorrect");
+      inputPhone.classList.add("Correct");
+    }
+    var regexText = /^[A-ZČĆŽŠĐ][a-zčćžšđ]([\d \w]{5,100})$/;
+    if (!regexText.test(text)) {
+      inputText.classList.remove("Correct");
+      inputText.classList.add("Incorrect");
+      errors.push(
+        "Please write a correct message! Example of a correctly written message:A message of 5 to 100 characters and first letter capital"
+      );
+    } else {
+      inputText.classList.remove("Incorrect");
+      inputText.classList.add("Correct");
+    }
+
+    errorHtml = errors.map((error) => {
+      return $(`<div>${error}</div>`);
+    });
+    $("#errors").empty();
+    $.each(errorHtml, function (i, val) {
+      $("#errors").append(val);
+    });
+
+    for (var i = 0; i < gender.length; i++) {
+      if (gender[i].checked == true) {
+        genValues = true;
+      }
+    }
+
+    if (!genValues) {
+      errors.push("Please choose the gender!");
+      return false;
+    }
+  });
+});
--- js/main.js
+++ js/main.js
@@ -65,7 +65,7 @@ window.onload = () => {
         "Founded back in 1969 as Samsung Electric Industries, Suwon, South Korea-headquartered Samsung Electronics",
     },
   ];
-
+  //dinamic print top 3 phones
   const generateTop3content = () => {
     //get parent element
     const top3div = document.querySelector(".grid_1_of_3");
@@ -83,6 +83,8 @@ window.onload = () => {
     }
   };
 
+  //call ajax
+
   function fetchData(url, method, callback) {
     $.ajax({
       url: url,
@@ -97,11 +99,15 @@ window.onload = () => {
     });
   }
 
+  //click on mark and show phone
+
   function clickMark(allData) {
     const categories = allData.filter((item) => {
       return item.mark === clickedText;
     });
   }
+
+  //dynamic phone printing
 
   function generateMobilePhonesContent(AllJsonData) {
     /*       const categories = AllJsonData.filter((item)=> {
@@ -114,6 +120,12 @@ window.onload = () => {
         <h2 class="saletitle">${item.mark} ${item.model}</h2>
         <h3 id="pricetitle">${item.price}</h3>
         <h2 class="saletitle">${item.sale}</h2>
+        <a class="aa-ad-card-btn" type="button" id="clickCart">
+            <span class="fa fa-shopping-cart">
+              
+            </span>
+            Add to cart
+            </a>
       </div>`;
     }
     if (!AllJsonData.length) {
@@ -122,6 +134,8 @@ window.onload = () => {
     document.querySelector("#content").innerHTML = html;
   }
 
+  //sort price
+
   function data() {
     const sortType = document.getElementById("sort").value;
     if (sortType == "asc") {
@@ -129,6 +143,53 @@ window.onload = () => {
     }
     return data.sort((a, b) => (a.price < b.price ? 1 : -1));
   }
+
+  //input search phone
+
+  const apiSearchBaseUrl = (searchTerm) => {
+    return `https://polar-thicket-29502.herokuapp.com/mobile${searchTerm}`;
+  };
+
+  const getSearchResults = async (searchTerm) => {
+    const results = await fetch(apiSearchBaseUrl(searchTerm), {
+      method: "GET",
+    });
+    return results.json();
+  };
+
+  const getPhone = (input) => {
+    if (input.length > 1) {
+      getSearchResults(input).then((data) => {
+        const filterOutNonCover = data.docs.filter((ele) => {
+          return ele.cover_i;
+        });
+
+        const topResults = filterOutNonCover.slice(0, 12);
+        suggestedPhone = [];
+        const finalResult = topResults.map((ele) => {
+          return genereatePhoneItem(
+            ele.title,
+            `https://polar-thicket-29502.herokuapp.com/mobile`
+          );
+        });
+
+        suggestedPhone.push(...finalResult);
+        searchJquery.empty();
+        $.each(suggestedPhone, function (i, val) {
+          searchJquery.append(val);
+        });
+      });
+    }
+  };
+
+  let suggestedPhone = [];
+  const searchJquery = $("#content");
+  const search = document.querySelector("#searchinput");
+  const searchButton = document.querySelector("#buttonSearch");
+
+  searchButton.addEventListener("click", () => {
+    getPhone(search.value);
+  });
 
   //Initialise page
   generateLinks();
@@ -156,5 +217,77 @@ window.onload = () => {
     }
     slides[slideIndex - 1].style.display = "block";
     setTimeout(showSlides, 1000); // Change image every 2 seconds
+  }
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 200) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function () {
+    document.body.scrollTop = 0; // safari
+    document.documentElement.scrollTop = 0; // mozilla abd chrome
+  });
+  document.addEventListener("DOMContentLoaded", () => {
+    document
+      .querySelector("#clickCart")
+      .addEventListener("click", function cart() {
+        if (typeof Storage !== "undefined") {
+          if (sessionStorage.clickcount) {
+            sessionStorage.clickcount = Number(sessionStorage.clickcount) + 1;
+          } else {
+            sessionStorage.clickcount = 1;
+          }
+          document.getElementById("result").innerHTML =
+            "U korpi imate: " + sessionStorage.clickcount + " proizvod/a";
+        } else {
+          document.getElementById("result").innerHTML =
+            "Ovaj pregledač nema local storage";
+        }
+      });
+  });
+
+  //FOOTER REGEX
+  document.addEventListener("DOMContentLoaded", () => {
+    document
+      .getElementById("go-button")
+      .addEventListener("click", function formfooter() {
+        var fooNews = document.getElementById("#footer-regex");
+        var fooNewsInput = document.getElementById("#footer-regex").value;
+
+        var regexNews = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+        if (!regexNews.test(fooNews)) {
+          fooNewsInput.classList.remove("Correct");
+          fooNewsInput.classList.add("Incorrect");
+          errors.push(
+            "Please write a correct mail! Example of a correctly written mail address: mariaelias@hotmail.com"
+          );
+        } else {
+          inputEmail.classList.remove("Incorrect");
+          inputEmail.classList.add("Correct");
+        }
+      });
+
+    errorHtml = errors.map((error) => {
+      return $(`<div>${error}</div>`);
+    });
+    $("#errors").empty();
+    $.each(errorHtml, function (i, val) {
+      $("#errors").append(val);
+    });
+  });
+
+  //LOCAL STORAGE
+
+  if (localStorage) {
+    $("#go-button").click(function () {
+      var mail = $("#footer-regex").val();
+      localStorage.setItem("mail-korisnik", mail);
+      alert("Uneti mail je snimljen na Local Storage! Ocekujte poruku!");
+    });
+  } else {
+    alert("Error! ");
   }
 };
